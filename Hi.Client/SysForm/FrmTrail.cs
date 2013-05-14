be8fd99c01d490c765b771dd95b1e09d18163763
Jose <gaozhenliang.2008@163.com>
2013-05-15 00:44:17 +0800

    Test

--- Hi.Client/SysForm/FrmTrail.cs
+++ Hi.Client/SysForm/FrmTrail.cs
@@ -32,7 +32,7 @@ namespace Hi.Client
 
         private void FrmTrail_Load(object sender, EventArgs e)
         {
-            //Test
+            //Test!
         }
 
         private void btnSumbit_Click(object sender, EventArgs e)
